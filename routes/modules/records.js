70bfcd57305ec2490f426c4b1c35b2ae576e39db
AlleyChang <yifanchang0602@gmail.com>
2022-08-30 15:47:11 +0800

    feat: add edit function and modify edit.hbs

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -9,10 +9,9 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const { name, date, amount, category } = req.body
-  Category.findOne({category })
+  Category.findOne({ category })
     .then(category => {
       const categoryId = category._id
-      console.log(categoryId)
       return Record.create({ 
         name, 
         date, 
@@ -28,9 +27,44 @@ router.post('/', (req, res) => {
 })
 
 //edit
-router.post('/edit', (req, res) => {
-  res.render('/edit')
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .lean()
+    .then(record => {
+      const categoryId = record.categoryId
+      return Category.findById(categoryId)
+        .lean()
+        .then(data => {
+          const category = data.name
+          res.render('edit', { record, category })
+        })
+    })
+    .catch(err => console.log(err))
 })
+
+router.post('/:id/edit', (req, res) => {
+  const { name, date, category, amount } = req.body
+  const id = req.params.id
+  Category.findOne({ category })
+    .then(category => {
+      const categoryId = category._id
+      return Record.findById(id)
+                  .then(record => {
+                    console.log('categoryId', categoryId)
+                    record.name = name
+                    record.date = date
+                    record.amount = amount
+                    record.categoryId = categoryId
+                    return record.save()
+                  })
+                  .then(() => res.redirect('/'))
+                  .catch(err => console.log(err))
+    }) 
+    .catch(err => console.log(err)) 
+})
+
+
 //delete
 
 module.exports = router
