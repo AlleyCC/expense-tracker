e24cc90ff4bbb933f5206cbc062bde9fc551ce8e
AlleyChang <yifanchang0602@gmail.com>
2022-09-05 17:11:35 +0800

    feat: modify sorting route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -19,11 +19,13 @@ router.get('/', (req, res) => {
 
 //search
 //sort
-router.get('/sort_household', (req, res) => {
+router.get('/sort', (req, res) => {
   const householdSelected = 'selected'
   const userId = req.user._id
+  const sort = req.query.sort
   let totalAmount = 0
-  Category.find({name: '家居物業'})
+  if (sort === 'household') {
+    Category.find({name: '家居物業'})
     .lean()
     .then(categoryData => {
       const categoryId = categoryData[0]._id
@@ -33,86 +35,70 @@ router.get('/sort_household', (req, res) => {
           records.map(record => {
             totalAmount += record.amount
           })
-          res.render('index', { records, householdSelected, totalAmount })
+          res.render('index', { records, householdSelected: 'selected', totalAmount })
         }) 
     })
+  } else if (sort === 'traffic') {
+    Category.find({name: '交通出行'})
+    .lean()
+    .then(categoryData => {
+      const categoryId = categoryData[0]._id
+      Records.find({categoryId, userId})
+        .lean()
+        .then(records => {
+          records.map(record => {
+            totalAmount += record.amount
+          })
+          res.render('index', { records, trafficSelected: 'selected', totalAmount })
+        }) 
+    })
+  } else if (sort === 'entertainment') {
+    Category.find({name: '休閒娛樂'})
+    .lean()
+    .then(categoryData => {
+      const categoryId = categoryData[0]._id
+      Records.find({categoryId, userId})
+        .lean()
+        .then(records => {
+          records.map(record => {
+            totalAmount += record.amount
+          })
+          res.render('index', { records, entertainmentSelected: 'selected', totalAmount })
+        }) 
+    })
+  } else if (sort === 'food') {
+    Category.find({name: '餐飲食品'})
+    .lean()
+    .then(categoryData => {
+      const categoryId = categoryData[0]._id
+      Records.find({categoryId, userId})
+        .lean()
+        .then(records => {
+          records.map(record => {
+            totalAmount += record.amount
+          })
+          res.render('index', { records, foodSelected: 'selected', totalAmount })
+        }) 
+    })
+  } else if (sort === 'others') {
+    Category.find({name: '其他'})
+    .lean()
+    .then(categoryData => {
+      const categoryId = categoryData[0]._id
+      Records.find({categoryId, userId})
+        .lean()
+        .then(records => {
+          records.map(record => {
+            totalAmount += record.amount
+          })
+          res.render('index', { records, othersSelected: 'selected', totalAmount })
+        }) 
+    })
+  }
+  
 })
 
-router.get('/sort_traffic', (req, res) => {
-  const trafficSelected = 'selected'
-  const userId = req.user._id
-  let totalAmount = 0
-  Category.find({name: '交通出行'})
-    .lean()
-    .then(categoryData => {
-      const categoryId = categoryData[0]._id
-      Records.find({categoryId, userId})
-        .lean()
-        .then(records => {
-          records.map(record => {
-            totalAmount += record.amount
-          })
-          res.render('index', { records, trafficSelected, totalAmount })
-        }) 
-    })
-})
 
-router.get('/sort_entertainment', (req, res) => {
-  const entertainmentSelected = 'selected'
-  const userId = req.user._id
-  let totalAmount = 0
-  Category.find({name: '休閒娛樂'})
-    .lean()
-    .then(categoryData => {
-      const categoryId = categoryData[0]._id
-      Records.find({categoryId, userId})
-        .lean()
-        .then(records => {
-          records.map(record => {
-            totalAmount += record.amount
-          })
-          res.render('index', { records, entertainmentSelected, totalAmount })
-        }) 
-    })
-})
-
-router.get('/sort_food', (req, res) => {
-  const foodSelected = 'selected'
-  const userId = req.user._id
-  let totalAmount = 0
-  Category.find({name: '餐飲食品'})
-    .lean()
-    .then(categoryData => {
-      const categoryId = categoryData[0]._id
-      Records.find({categoryId, userId})
-        .lean()
-        .then(records => {
-          records.map(record => {
-            totalAmount += record.amount
-          })
-          res.render('index', { records, foodSelected, totalAmount })
-        }) 
-    })
-})
-
-router.get('/sort_others', (req, res) => {
-  const othersSelected = 'selected'
-  const userId = req.user._id
-  let totalAmount = 0
-  Category.find({name: '其他'})
-    .lean()
-    .then(categoryData => {
-      const categoryId = categoryData[0]._id
-      Records.find({categoryId, userId})
-        .lean()
-        .then(records => {
-          records.map(record => {
-            totalAmount += record.amount
-          })
-          res.render('index', { records, othersSelected, totalAmount })
-        }) 
-    })
-})
 
 
 module.exports = router
